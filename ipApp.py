3f353c7bc008a21dfcdb6b5b7dd937fdc953a713
qjfcaseo <qjfcaseo@tip.edu.ph>
2022-10-27 13:12:46 +0000

    working

--- ipApp.py
+++ ipApp.py
@@ -3,46 +3,66 @@ from requests import get
 import requests 
 
 app = Flask(__name__)
-response = get('https://ipapi.co/json/').json()
 
 # IP Address (IPv4)
 @app.route('/ip', methods = ['GET'])
 def get_ip():
-    return {'IP Address': response['ip']}
+    # return {'IP Address (IPv4)': apiResponse['ip']}
+    ip_api = requests.get('https://ipapi.co/json/').json()
+    return ip_api['ip']
+    
+ip_address = get_ip()
+response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+
 
 # ASN
 @app.route('/asn', methods = ['GET'])
 def get_asn():
-        return {'ASN' : response['asn']}
+        return {'ASN' : apiResponse['asn']}
 
 
 # CountryCode
 @app.route('/countrycode', methods = ['GET'])
 def get_countryCode():
-    response = get('https://ipapi.co/json/').json()
-    return {'Country' : response['country_code']}
+    return {'Country Code' : apiResponse['country_code']}
 
 # Geolocation
+# def get_ip():
+#     response = requests.get('https://api64.ipify.org?format=json').json()
+#     return response["ip"]
 @app.route('/geoloc', methods = ['GET'])
-def get_myip():
-        response = get('https://api64.ipify.org?format=json').json()
-        return response["ip"]
 def get_geoloc():
-        ip_address = get_myip()
-        response =  get(f'https://ipapi.co/{ip_address}/json/').json()
-        location_data = {
-            "country": response.get("country_name"),
-            "continent_code": response.get("continent_code"),
-            "region": response.get("region"),
-            "country_capital": response.get("country_capital"),
-            "city": response.get("city"),
-            "latitude": response.get("latitude"),
-            "longitude": response.get("longitude")
-        }
-        # return {'Country' : response['country_name'], 
-        # "continent_code": response["continent_code"],
-        # "region": response["region"]}
-        return location_data
+    # ip_address = get_ip()
+    # response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "Country": response.get("country_name"),
+        "Continent_code": response.get("continent_code"),
+        "Region": response.get("region"),
+        "Country_capital": response.get("country_capital"),
+        "City": response.get("city"),
+        "Latitude": response.get("latitude"),
+        "Longitude": response.get("longitude")
+    }
+    return location_data
+
+# All
+@app.route('/ipApp', methods = ['GET'])
+def get_all():
+    # ip_address = get_ip()
+    # response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    data = {
+        'IP Address (IPv4)': response.get('ip'),
+        'ASN' : response.get('asn'),
+        'Country Code' : response.get('country_code'),
+        "Country": response.get("country_name"),
+        "Continent_code": response.get("continent_code"),
+        "Region": response.get("region"),
+        "Country_capital": response.get("country_capital"),
+        "City": response.get("city"),
+        "Latitude": response.get("latitude"),
+        "Longitude": response.get("longitude")
+    }
+    return data
 
 
 if __name__ == "__main__":
