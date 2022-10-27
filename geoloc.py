0fc3548dd582a83473564e7621d0fefd1c5ead87
qammisip <qammisip@tip.edu.ph>
2022-10-27 08:30:46 +0000

    Nothing new

--- geoloc.py
+++ geoloc.py
@@ -1,5 +1,22 @@
-# Gets geo location of the computer (Angel)
-from requests import get
+# Gets geo location of the computer using API (ipapi.co)
 
-loc = get('https://ipapi.co/json/')
-print (loc.json())
+import requests
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
+
+
+print(get_location())
