829e1d0ee6a086dc3a156292431b29041a16e5fb
qjfcaseo <qjfcaseo@tip.edu.ph>
2022-10-27 10:33:53 +0000

    ipAdd.py Final

--- ipAdd.py
+++ ipAdd.py
@@ -1,11 +1,24 @@
 
-# loc = get('https://ipapi.co/json/')
-# print (loc.json())
-# from flask import Flask
+
+from flask import Flask
 from requests import get
 
+app = Flask(__name__)
 
+response = get('https://ipapi.co/json/').json()
+
+@app.route('/ip', methods = ['GET'])
 def get_ip():
-    response = get('https://ipapi.co/json/').json()
-    return response["ip"]
-print(get_ip())
+    return {'IP Address': response['ip']}
+
+    
+
+
+if __name__ == "__main__":
+    app.run()
+
+# =================================================
+# def get_ip():
+#     response = get('https://ipapi.co/json/').json()
+#     return response["ip"]
+# print(get_ip())
--- ipApp.py
+++ ipApp.py
@@ -1,3 +1,12 @@
-
 # call everyone here
+from flask import Flask
+from requests import get
+
+
+app = Flask (__name__)
+
+
+if __name__ == "__main__":
+    app.run()
+
 
